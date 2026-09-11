f46307f7b85bf6c5c8444cd0529212668a181517
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-11 09:24:06 +0200

    Add Question3

---
+++ Question3.java
@@ -0,0 +1,14 @@
+//Esli Quest Esabu 24019733
+public class Question3{
+    public static void main(String[] args){
+
+        //Starting with the first even number
+        int number = 2;
+
+        //printing even numbers until >= 5-
+        while(number <= 50){
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
