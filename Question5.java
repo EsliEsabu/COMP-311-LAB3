fb68414b5d9a5b2d72bfae83e3688d9fa0baa1d6
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-11 09:25:56 +0200

    Add Question5

--- Question5.java
+++ Question5.java
@@ -1,3 +1,16 @@
+//Esli Quest Esabu 24019733 
+import java.util.Scanner;
 public class Question5 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        do { 
+            //ask untill a  valid number is added
+            System.out.println("Enter a number between 1 and 10:");
+            number = input.nextInt();
+
+        } while (number < 1 || number > 10);
+        System.out.println("Valid number:" +number);   }
     
 }
