8d6f1586c86dca89c981e5822ca72f48f665de67
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-11 09:27:34 +0200

    Add Question6

--- Question6.java
+++ Question6.java
@@ -1,1 +1,21 @@
+//Esli Quest Esabu
 
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        int total = 0;
+
+        do {
+            System.out.println("Enter a number (0 to stop):");
+            number = input.nextInt();
+            total += number;
+        } while (number != 0);
+
+        System.out.println("Final total: " + total);
+
+        input.close();
+    }
+}
